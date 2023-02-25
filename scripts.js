2a9fefac049e396039d222fa6c6ddb56ca07f1ab
Abhishek Kumar Singh <abhishekse@outlook.com>
2023-02-25 15:19:56 +0530

    added functionality to render data dynamically in the page

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 //detect user click search button
 let btnClick = document.getElementById("search-btn");
-btnClick.addEventListener("click", searchBook);
+btnClick.addEventListener("click", renderBooks);
 
 //detect if user pressed enter to search
 let input = document.getElementById("search-input");
@@ -12,13 +12,56 @@ input.addEventListener("keypress", (Event) => {
     btnClick.click();
   }
 });
-function searchBook() {
-    let bookName=input.value;
+
+//array to store results
+let cards = [];
+
+//fuction to fetch data aand
+function renderBooks() {
+  let bookName = input.value;
+  console.log(bookName);
   fetch("https://www.googleapis.com/books/v1/volumes?q=" + bookName)
-    .then((response) => 
-      response.json()
-    )
+    .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      let len = data.items.length;
+      //dyanimcally creating cards for each response.
+      console.log("FETCHING BOOKS");
+      data.items.forEach((book) => {
+        let temp = `<div class="col-sm">
+        <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="${book.volumeInfo.imageLinks.thumbnail}" alt="no image">
+                    <div class="card-body">
+                      <h5 class="card-title">${book.volumeInfo.title}</h5>
+                      <h6>${book.volumeInfo.authors?book.volumeInfo.authors:''}</h6>
+                    </div>
+                  </div>
+                  </div>
+                  `;
+        cards.push(temp);
+      });
+      //testing
+    //   console.log("FETCHED BOOKS");
+    //   console.log("Creting UI");
+      let numRows = Math.floor(len / 3) + 1;
+      let list = "";
+      let x = 0;
+      for (i = 0; i < numRows; i++) {
+        //creating a row.
+        let row = '<div class="row">';
+        for (; x < len; x++) {
+          row += cards[x];
+        }
+        row += "</div>";
+        list += row;
+      }
+      document.getElementById("books").innerHTML = list;
+    }).finally(()=>{
+        // console.log("Hello") //testing
+        cards=[]; //resetting so that logic works for new requests
     });
 }
+//display cards for testing
+//console.log(cards);
+
+//show one card.
+// console.log(cards[0])
